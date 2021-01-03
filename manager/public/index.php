2c690b34f75e1a2cf88a833827bab2c73a161d63
dev-iskan <successfuliskan@gmail.com>
2021-01-03 22:37:34 +0500

    added slim and optimized for production

--- manager/public/index.php
+++ manager/public/index.php
@@ -1,11 +1,29 @@
 <?php
 
-echo '<html lang="en">
-<head>
-    <title>App</title>
-    <meta charset="UTF-8">
-</head>
-<body>
-    <h1>Hello World</h1>
-</body>
-</html>';
+declare(strict_types=1);
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+chdir(dirname(__DIR__));
+require 'vendor/autoload.php';
+
+http_response_code(500);
+
+(function () {
+    $app = new \Slim\App([
+        'settings' => [
+            'addContentLengthHeader' => false,
+            'displayErrorDetails' => (bool)getenv('APP_DEBUG'),
+        ],
+    ]);
+
+    $app->get('/', function (Request $request, Response $response) {
+        return $response->withJson([
+            'name' => 'Manager1',
+            'param' => $request->getQueryParam('param'),
+        ]);
+    });
+
+    $app->run();
+})();
