b36cbda6f9ce355a66124ceae271110a2e64975c
dev-iskan <successfuliskan@gmail.com>
2021-01-14 23:49:34 +0500

    removed unused controllers and templates

--- manager/src/Controller/HomeController.php
+++ manager/src/Controller/HomeController.php
@@ -9,12 +9,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
+     * @return Response
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('app/home.html.twig');
     }
 }
