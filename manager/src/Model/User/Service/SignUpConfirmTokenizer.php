0b49f2806875cce63cde857c65e166e106785853
dev-iskan <successfuliskan@gmail.com>
2021-01-17 01:44:27 +0500

    added basic service logic

---
+++ manager/src/Model/User/Service/SignUpConfirmTokenizer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Model\User\Service;
+
+use Ramsey\Uuid\Uuid;
+
+class SignUpConfirmTokenizer
+{
+    public function generate(): string
+    {
+        return Uuid::uuid4()->toString();
+    }
+}
